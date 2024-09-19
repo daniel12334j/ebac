f25257752b1ce4f030e7bbacc706e129b5f5aeb0
Daniel Junior <daniel-juininho@outlook.com>
2024-09-19 11:28:18 -0300

    Add files via upload

---
+++ oo.ts
@@ -0,0 +1,25 @@
+function multiplicando(n1: number, n2: number){
+    return n1 * n2
+}
+
+
+const resultado = multiplicando(6,5);
+
+function saudacao(nome: string){        //ou      function saudacao(nome: string){
+                                                    //  return `olá ` + nome;
+                                                    // }
+    return `olá ${nome}`
+};
+
+
+
+// 
+
+
+
+
+const nome = saudacao('Daniel');
+
+console.log(nome);
+console.log(resultado);
+
